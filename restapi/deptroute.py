a4ac0303504ab460c2e70ecf9d283caebbc7a9b9
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-12-29 10:45:44 +0530

    post-commit

--- restapi/deptroute.py
+++ restapi/deptroute.py
@@ -1,9 +1,21 @@
 from orm.models import Department
 from config import db,app
-from flask import jsonify
+from flask import jsonify,request,abort
 
 @app.route("/departments",methods=['GET'])
 def getDepartments():
     departments=Department.query.all()
     departments=[ x.serialize() for x in departments]
     return jsonify(departments)
+
+@app.route("/departments",methods=['POST'])
+def processDepartments():
+    try:
+        data=request.get_json()
+        print(data)
+        department=Department(data['name'])
+        db.session.add(department)
+        db.session.commit()
+        return {"status": "success"}, 201
+    except:
+        abort({'status':"Internal server error"},500)
